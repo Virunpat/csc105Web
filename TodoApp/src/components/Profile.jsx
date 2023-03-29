219fd8b845e1bb24e041e41494cc2815e419ebd5
Virunpat <119098874+Virunpat@users.noreply.github.com>
2023-03-29 22:54:05 +0700

    first commit

---
+++ TodoApp/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import "./Profile.css";
+
+function Profile() {
+  return (
+	<div className="layout">
+		<img src="src/assets/images.jfif" alt="profile" />
+		<h1 className="name" >Virunpat Theeranuluk</h1>
+		<hr></hr>
+	</div>
+  )
+}
+
+export default Profile
